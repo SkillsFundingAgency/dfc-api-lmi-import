50e0e2d99cb461af8d777f7c46e78b2237a671da
Ian Crisp <icrisp@program-systems.co.uk>
2021-03-22 07:28:22 +0000

    Increased retry from 5 to 10 to allow job profiles to gets handle 429 errors

--- DFC.Api.Lmi.Import/HttpClientPolicies/RetryPolicyOptions.cs
+++ DFC.Api.Lmi.Import/HttpClientPolicies/RetryPolicyOptions.cs
@@ -5,7 +5,7 @@ namespace DFC.Api.Lmi.Import.HttpClientPolicies
     [ExcludeFromCodeCoverage]
     public class RetryPolicyOptions
     {
-        public int Count { get; set; } = 5;
+        public int Count { get; set; } = 10;
 
         public int BackoffPower { get; set; } = 2;
     }
