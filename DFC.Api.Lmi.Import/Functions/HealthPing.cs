1fc76f3a2a3e63245205c9889318c31b38f0c929
Ian Crisp <49161034+IanCrisp@users.noreply.github.com>
2021-11-15 12:11:08 +0000

    removed use of async (#65)

--- DFC.Api.Lmi.Import/Functions/HealthPing.cs
+++ DFC.Api.Lmi.Import/Functions/HealthPing.cs
@@ -6,7 +6,6 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
-using System.Threading.Tasks;
 
 namespace DFC.Api.Lmi.Import.Functions
 {
@@ -15,7 +14,7 @@ namespace DFC.Api.Lmi.Import.Functions
         [FunctionName("HealthPing")]
         [Display(Name = "Health ping", Description = "Simple OK response to a health ping")]
         [Response(HttpStatusCode = (int)HttpStatusCode.OK, Description = "OK", ShowSchema = false)]
-        public static async Task<IActionResult> Run(
+        public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "health/ping")] HttpRequest req, ILogger logger)
         {
             logger.LogInformation($"pinged");
